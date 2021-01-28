b4343058d973ffaab163160e2312c29c716afcdc
josilob <bjosilo@gmail.com>
2021-01-27 23:17:00 -0500

    small tweak

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,17 +3,14 @@ import React from 'react';
 const Favorites = (props) => {
 	const favesLoaded = props.faveSongs.map((fave, index) => {
 		return (
-			<>
-				<div className='songs' key={fave.id}>
-					<p className='title'>Title : {fave.title}</p>
-					<p className='artist'>Artist : {fave.artist}</p>
-					<p className='time'>Duration : {fave.time}</p>
-					{/* Button that toggles favorites on click in FAV LIST */}
-					<button>
-						<i className='fas fa-heart'></i>
-					</button>
-				</div>
-			</>
+			<div className='songs' key={fave.id}>
+				<p className='title'>Title : {fave.title}</p>
+				<p className='artist'>Artist : {fave.artist}</p>
+				<p className='time'>Duration : {fave.time}</p>
+				<button>
+					<i className='fas fa-heart'></i>
+				</button>
+			</div>
 		);
 	});
 	const loading = 'Add some songs to favorite section!';
